8a2bfd6d4d7e34a601b123c330e0dc5110e7fb15
kalashnikovaYla <kalashnikova-yla@mail.ru>
2023-04-18 20:51:38 +0500

    Initial Commit

---
+++ SettingsSwiftUI/SettingsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SettingsSwiftUIApp.swift
+//  SettingsSwiftUI
+//
+//  Created by sss on 18.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct SettingsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
